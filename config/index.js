9ed48365dc1c1c9288d6c0a411a0dbe13e881f17
AlfieriChou <alfierichou@gmail.com>
2018-10-17 14:05:01 +0800

    config

---
+++ config/index.js
@@ -0,0 +1,17 @@
+const _ = require('lodash')
+const development = require('./default.config')
+const release = require('./release.config')
+const production = require('./prod.config')
+
+const env = process.env.NODE_ENV || 'development'
+const configs = {
+  development: development,
+  production: production,
+  release: release
+}
+const defaultConfig = {
+  env: env
+}
+const config = _.merge(defaultConfig, configs[env])
+
+module.exports = config
