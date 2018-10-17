2cd1af69c9cc8571afbce4c3b1cb5d8cce4fddb6
AlfieriChou <alfierichou@gmail.com>
2018-10-17 14:15:00 +0800

    migration config

--- migration/index.js
+++ migration/index.js
@@ -1,16 +1,23 @@
 const knex = require('knex')
-const config = {
+const path = require('path')
+
+let env = process.argv[2]
+if (!env) env = 'default'
+const configPath = path.join(__dirname, '../config', `${env}.config.js`)
+const config = require(configPath)
+
+const knexConfig = {
   client: 'mysql',
   connection: {
-    host: '106.15.230.136',
-    user: 'lvyang',
-    password: 'zhazhayang',
-    database: 'test',
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database,
     supportBigNumbers: true,
     charset: 'utf8mb4',
     connectTimeout: 15000
   }
 }
-const db = knex(config)
+const db = knex(knexConfig)
 
 module.exports = db
